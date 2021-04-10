e4d8fa8ce72d065bf0903d3093b33bab2de9622e
Eric Meehan <eric.meehan@homodeus.us>
2021-04-10 10:21:50 -0400

    Initial Commit

---
+++ blockchain/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  blockchain
+//
+//  Created by Eric Meehan on 4/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
