0bb1219fd0365763c1520e76d4e02a1ab491b6ec
Eric Meehan <eric.meehan@homodeus.us>
2021-05-29 13:39:36 -0400

    Post-Stream

--- blockchain/blockchain.c
+++ blockchain/blockchain.c
@@ -8,19 +8,226 @@
 // blockchain.c
 //
 // Eric Meehan
-// 5/12/21
+// 5/29/21
 //
 //
 
 #include "blockchain.h"
 
-#include "interfaces/cli.h"
-
-
-int main(int argc, const char **argv)
+int BLOCKCHAIN_account_create(char *username)
 {
-    if (isatty(fileno(stdin)))
+    EVP_PKEY *key = EVP_PKEY_new();
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    
+    EVP_PKEY_keygen_init(ctx);
+    EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 4096);
+    EVP_PKEY_keygen(ctx, &key);
+    
+    char path[64] = {0};
+    strcat(path, PROFILE_PATH);
+    strcat(path, username);
+    strcat(path, ".pem");
+    
+    FILE *key_file = fopen(path, "w");
+    if (!PEM_write_PrivateKey(key_file, key, EVP_aes_256_cbc(), NULL, 0, NULL, NULL))
     {
-        return BLOCKCHAIN_cli(argc, argv);
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to write private key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_CTX_free(ctx);
+        return EXIT_FAILURE;
     }
+    fclose(key_file);
+    
+    memset(path, 0, 64);
+    strcat(path, PROFILE_PATH);
+    strcat(path, username);
+    strcat(path, ".der");
+    
+    key_file = fopen(path, "w");
+    
+    if (!i2d_PUBKEY_fp(key_file, key))
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to write public key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    fclose(key_file);
+    EVP_PKEY_CTX_free(ctx);
+    return EXIT_SUCCESS;
+}
+
+int BLOCKCHAIN_account_login(BLOCKCHAIN_OBJ_Account *account, char *username)
+{
+    account->public_key = EVP_PKEY_new();                               // REMEMBER TO FREE THESE
+    account->private_key = EVP_PKEY_new();
+    
+    char path[64] = {0};
+    strcat(path, PROFILE_PATH);
+    strcat(path, username);
+    strcat(path, ".der");
+    
+    FILE *key_file = fopen(path, "r");
+    if (!key_file)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Could not find public key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_free(account->private_key);
+        EVP_PKEY_free(account->public_key);
+        return EXIT_FAILURE;
+    }
+    
+    if (!d2i_PUBKEY_fp(key_file, &account->public_key))
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to load public key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_free(account->private_key);
+        EVP_PKEY_free(account->public_key);
+        return EXIT_FAILURE;
+    }
+    fprintf(stdout, "BLOCKCHAIN: Successfully loaded public key for account %s\n", username);
+    fclose(key_file);
+    
+    memset(path, 0, 64);
+    strcat(path, PROFILE_PATH);
+    strcat(path, username);
+    strcat(path, ".pem");
+    
+    key_file = fopen(path, "r");
+    if (!key_file)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Could not find private key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_free(account->private_key);
+        EVP_PKEY_free(account->public_key);
+        return EXIT_FAILURE;
+    }
+    
+    if (!PEM_read_PrivateKey(key_file, &account->private_key, NULL, NULL))
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to load private key for account %s\n", username);
+        fclose(key_file);
+        EVP_PKEY_free(account->private_key);
+        EVP_PKEY_free(account->public_key);
+        return EXIT_FAILURE;
+    }
+    
+    fprintf(stdout, "BLOCKCHAIN: Successfully loaded private key for account %s\n", username);
+    return EXIT_SUCCESS;
+}
+
+int BLOCKCHAIN_block_hash(BLOCKCHAIN_OBJ_Block *block, byte *digest)
+{
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    if (EVP_DigestInit_ex(ctx, EVP_sha512(), NULL) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to initialize EVP context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    if (EVP_DigestUpdate(ctx, block, block->headers.size) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to update EVP context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    unsigned int size = 64;
+    if (EVP_DigestFinal(ctx, digest, &size) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to finalize EVP context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    EVP_MD_CTX_free(ctx);
+    return EXIT_SUCCESS;
+}
+
+int BLOCKCHAIN_block_calculate_difficulty(BLOCKCHAIN_OBJ_Block *block)
+{
+    return pow(log(sqrt((double)block->headers.size)), 2) + 1;
+}
+
+bool BLOCKCHAIN_block_proof_of_work(BLOCKCHAIN_OBJ_Block *block)
+{
+    BN_CTX *ctx = BN_CTX_new();
+    
+    BIGNUM *base = BN_new();
+    BN_dec2bn(&base, "2");
+    
+    BIGNUM *exponent = BN_new();
+    int difficulty = 512 - BLOCKCHAIN_block_calculate_difficulty(block);
+    char exp[8] = {0};
+    sprintf(exp, "%d", difficulty);
+    BN_dec2bn(&exponent, exp);
+    
+    BIGNUM *target = BN_new();
+    BN_exp(base, exponent, target, ctx);
+    
+    byte digest[64];
+    BLOCKCHAIN_block_hash(block, digest);
+    BIGNUM *value = BN_new();
+    BN_bin2bn(digest, 64, value);
+    
+    bool result = BN_cmp(target, value) == 1;
+    
+    BN_free(base);
+    BN_free(exponent);
+    BN_free(target);
+    BN_free(value);
+    BN_CTX_free(ctx);
+    
+    return result;
+}
+
+
+int BLOCKCHAIN_block_create(BLOCKCHAIN_OBJ_Account *account, BLOCKCHAIN_OBJ_Block *previous, byte *data, unsigned long size)
+{
+    BLOCKCHAIN_OBJ_Block *block = malloc(sizeof(BLOCKCHAIN_OBJ_BlockHeaders) + size);
+    
+    i2d_PUBKEY(account->public_key, (unsigned char **)&block->headers.author);
+    block->headers.size = sizeof(BLOCKCHAIN_OBJ_BlockHeaders) + size;
+    memcpy(&block->data, data, size);
+    BLOCKCHAIN_block_sign(account, block);
+    
+    if (BLOCKCHAIN_block_hash(previous, block->headers.previous) != EXIT_SUCCESS)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to hash previous block\n");
+        free(block);
+        return EXIT_FAILURE;
+    }
+    
+    block->headers.nonce = 0;
+    while (!BLOCKCHAIN_block_proof_of_work(block)) { block->headers.nonce += 1; }
+    
+    
+}
+
+int BLOCKCHAIN_block_sign(BLOCKCHAIN_OBJ_Account *account, BLOCKCHAIN_OBJ_Block *block)
+{
+    memset(block->headers.signature, 0, 512);
+    
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    
+    if (EVP_DigestSignInit(ctx, NULL, EVP_sha512(), NULL, account->private_key) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to initialize signature context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    if (EVP_DigestSignUpdate(ctx, &block->data, block->headers.size - sizeof(BLOCKCHAIN_OBJ_BlockHeaders)) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to update signature context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    size_t siglen;
+    if (EVP_DigestSignFinal(ctx, block->headers.signature, &siglen) <= 0)
+    {
+        fprintf(stderr, "BLOCKCHAIN ERROR: Failed to initialize signature context\n");
+        EVP_MD_CTX_free(ctx);
+        return EXIT_FAILURE;
+    }
+    fprintf(stdout, "BLOCKCHAIN: Successfully signed block\n");
+    EVP_MD_CTX_free(ctx);
+    return EXIT_SUCCESS;
 }
--- blockchain/blockchain.h
+++ blockchain/blockchain.h
@@ -8,147 +8,54 @@
 // blockchain.h
 //
 // Eric Meehan
-// 5/10/21
+// 5/29/21
 //
 //
-
-/*
- A top level header file for the blockchain library.
- Include this file when building third party applications in C.
- */
 
 #ifndef blockchain_h
 #define blockchain_h
 
-#include <libeom/libeom.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include <openssl/bn.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rsa.h>
 
-#include "src/mongoose/mongoose.h"
-#include "src/sqlite/sqlite3.h"
+const char *PROFILE_PATH = "/Users/eric/Desktop/";
 
-#include <math.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-#include <uuid/uuid.h>
+typedef unsigned char byte;
 
-static const char *PIPE_PATH = "/Users/eric/Desktop/";
-static const char *PROFILE_PATH = "/Users/eric/Desktop/";
+typedef struct BLOCKCHAIN_OBJ_BlockHeaders
+{
+    byte author[550];
+    byte signature[512];
+    unsigned long nonce;
+    unsigned long size;
+    byte previous[64];
+} __attribute__((packed)) BLOCKCHAIN_OBJ_BlockHeaders;
+ 
+typedef struct BLOCKCHAIN_OBJ_Block
+{
+    BLOCKCHAIN_OBJ_BlockHeaders headers;
+    byte data;
+} __attribute__((packed)) BLOCKCHAIN_OBJ_Block;
 
-static const char *LOG_PATH = "/Users/eric/Desktop/";
+typedef struct BLOCKCHAIN_OBJ_Account
+{
+    EVP_PKEY *public_key;
+    EVP_PKEY *private_key;
+} BLOCKCHAIN_OBJ_Account;
 
-static const unsigned int NUM_THREADS = 32;
+int BLOCKCHAIN_block_create(BLOCKCHAIN_OBJ_Account *account, BLOCKCHAIN_OBJ_Block *previous, byte *data, unsigned long size);
+int BLOCKCHAIN_block_hash(BLOCKCHAIN_OBJ_Block *block, byte *digtest);
+bool BLOCKCHAIN_block_proof_of_work(BLOCKCHAIN_OBJ_Block *block);
+int BLOCKCHAIN_block_calculate_difficulty(BLOCKCHAIN_OBJ_Block *block);
+int BLOCKCHAIN_block_sign(BLOCKCHAIN_OBJ_Account *account, BLOCKCHAIN_OBJ_Block *block)
 
-static char *BLOCK_PATH = "/Users/eric/Desktop/";
-static const unsigned long MAX_BLOCK_SIZE = 1000000000;
-
-struct mg_mgr SERVER;
-static const char *URL = "tcp://0.0.0.0.0:%d/";
-static const unsigned int PORT = 1248;
-static const int SERVER_PULSE = 100;;
-static const char *SERVER_LOGGING_FORMAT = "{"
-"\"session\": \"%s\", "
-"\"client\": { "
-"\t\"ip\": %lu, "
-"\t\"ip6\": %lu, "
-"\t\"is_ip6\": %d, "
-"\t\"port\": %lu "
-"\t} "
-"\"status\": %d, "
-"\"event\": %d, "
-"\"route\": \"%s\", "
-"\"response\", %d, "
-"\"notes\": \"%s\""
-"}\n";
-
-struct mg_mgr *CLIENT;
-static const int CLIENT_PULSE = 1000;
-static const char *CLIENT_LOGGING_FORMAT = "{"
-"\"session\": \"%s\", "
-"\"server\": { "
-"\t\"ip\": %lu, "
-"\t\"ip6\": %lu, "
-"\t\"is_ip6\": %d, "
-"\t\"port\": %lu "
-"\t} "
-"\"status\": %d, "
-"\"event\": %d, "
-"\"notes\": \"%s\""
-"}\n";
-
-static const char *DATABASE_PATH = "/Users/eric/Desktop/blockchain.db";
-static sqlite3 *DATABASE;
-static const unsigned int MAX_SQL = 4096;
-
-
-
-
-static const char *HELP = "\n"
-"\n"
-"\n"
-"\e[36m ________  ___       ________  ________  ___  __    ________  ___  ___  ________  ___  ________      \n"
-"|\\   __  \\|\\  \\     |\\   __  \\|\\   ____\\|\\  \\|\\  \\ |\\   ____\\|\\  \\|\\  \\|\\   __  \\|\\  \\|\\   ___  \\    \n"
-"\\ \\  \\|\\ /\\ \\  \\    \\ \\  \\|\\  \\ \\  \\___|\\ \\  \\/  /|\\ \\  \\___|\\ \\  \\\\\\  \\ \\  \\|\\  \\ \\  \\ \\  \\\\ \\  \\   \n"
-" \\ \\   __  \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\    \\ \\   ___  \\ \\  \\    \\ \\   __  \\ \\   __  \\ \\  \\ \\  \\\\ \\  \\  \n"
-"  \\ \\  \\|\\  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\____\\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \\  \\ \\  \\ \\  \\ \\  \\\\ \\  \\ \n"
-"   \\ \\_______\\ \\_______\\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\ \\__\\ \\__\\ \\__\\ \\__\\\\ \\__\\\n"
-"    \\|_______|\\|_______|\\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|\\|__|\\|__|\\|__|\\|__| \\|__|\e[0m\n"
-"\n"
-"\n"
-"Created by Eric O Meehan\n"
-"https://www.ericomeehan.com\n"
-"\n"
-"\n"
-"\e[1mGENERAL USAGE\e[0m\n"
-"\e[32mblockchain\n"
-"    \e[32maccount\e[0m                                 account management\n"
-"        \e[32mcreate\e[0m                                      create a new account\n"
-"            \e[2m[username]\e[0m\n"
-"\n"
-"    \e[32mcreate\e[0m                                  content creation\n"
-"        \e[32mfrom\e[0m                                        create a block from file contents\n"
-"            \e[2m[/path/to/file(s)]\e[0m                                 (can specify multiple files)\n"
-"        \e[32mthis\e[0m                                        create a block from incoming data\n"
-"            'content'                                                  (use quotes, can specify multiple)\n"
-"            \e[2m[size of content]\e[0m \e[2m[raw data]\e[0m               (not for general use)\n"
-"\n"
-"    \e[32mshare\e[0m                                   share content with the network\n"
-"        \e[2m[hash(es)] (\e[0mwith \e[2mip_address(es)\e[0m)            specify block(s) to share and, optionally, who to share with\n"
-"\n"
-"    \e[32mread\e[0m                                    read data from blocks\n"
-"        \e[32mblock\e[0m                                       read entire block\n"
-"            \e[2mhash(es)\e[0m\n"
-"        \e[32mheaders\e[0m                                     read only headers\n"
-"            \e[2mhash(es)\e[0m\n"
-"        \e[32mdata\e[0m                                        read data only (general use case)\n"
-"            \e[2mhash(es)\e[0m\n"
-"\n"
-"    \e[32mquery\e[0m                                   search the blockchain network\n"
-"        \e[32mblock\e[0m                                       search for a specific block on the network by hash\n"
-"            \e[2mhash(es) (\e[0mfrom \e[2mip_address(es)\e[0m)\n"
-"        \e[32msql\e[0m                                         perform an sql query on the network\n"
-"            \e[2m[sql] (\e[0mfrom \e[2mip_address(es)\e[0m)\n"
-"\n"
-"\n"
-"\e[1mTYPICAL STARTUP\e[0m\n"
-"In order to utilize blockchain, you will need to first create and activate an account, then launch the blockchain server\n"
-"If you haven’t already, create an account using:\n"
-"    \e[7mblockchain account create \e[2musername\e[0m\n"
-"Now activate your account:\n"
-"    \e[7mblockchain account activate \e[2musername\e[0m\n"
-"And launch the server:\n"
-"    \e[7mblockchain start\e[0m\n"
-"\n"
-"For more detailed documentation, consult the README:       \e[34mhttps://github.com/ericomeehan/blockchain\e[0m\n"
-"Or visit Eric’s YouTube channel for tutorials:             \e[34mhttps://youtube.com/c/EricOMeehan\e[0m\n"
-"\n";
+int BLOCKCHAIN_account_create(char *username);
+int BLOCKCHAIN_account_login(BLOCKCHAIN_OBJ_Account *account, char *username);
 
 #endif /* blockchain_h */
